4ba14b8e7f3ea7a352cff4bde4b7bce2d4b45451
okezino <okezi002@gmail.com>
2022-10-21 16:54:27 +0100

    multi module gradule setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,22 +11,21 @@
 //}
 
 buildscript {
-    ext {
-        compose_version = '1.1.0-beta01'
-    }
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath "com.android.tools.build:gradle:7.0.4"
-        classpath 'org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31'
+        classpath(Build.androidBuildTools)
+        classpath(Build.hiltAndroidGradlePlugin)
+        classpath(Build.kotlinGradlePlugin)
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class){
+    delete(rootProject.buildDir)
 }
