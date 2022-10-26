76ecb59c8db38a4b395b940ae41fb239e2aa3f1b
okezino <okezi002@gmail.com>
2022-10-26 01:55:26 +0100

    navigation setup

--- app/src/main/java/com/example/multimoduleapplication/MainActivity.kt
+++ app/src/main/java/com/example/multimoduleapplication/MainActivity.kt
@@ -3,15 +3,17 @@ package com.example.multimoduleapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.core.navigation.Route
+import com.example.multimoduleapplication.navigate.navigate
 import com.example.multimoduleapplication.ui.theme.MultiModuleApplicationTheme
-import com.example.onboarding_presentation.welcome.welcomeScreen
+import com.example.onboarding_presentation.welcome.WelcomeScreen
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +21,40 @@ class MainActivity : ComponentActivity() {
         setContent {
             MultiModuleApplicationTheme {
                 // A surface container using the 'background' color from the theme
-              welcomeScreen()
+              val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = Route.WELCOME){
+                    composable(Route.ACTIVITY){
+
+                    }
+                    composable(Route.WELCOME){
+                     WelcomeScreen(onNavigate = navController::navigate)
+                    }
+                    composable(Route.AGE){
+
+                    }
+                    composable(Route.GOAL){
+
+                    }
+                    composable(Route.GENDER){
+
+                    }
+                    composable(Route.HEIGHT){
+
+                    }
+                    composable(Route.NUTRIENT_GOAL){
+
+                    }
+                    composable(Route.SEARCH){
+
+                    }
+                    composable(Route.TRACKER_OVERVIEW){
+
+                    }
+
+                    composable(Route.WEIGHT){
+
+                    }
+                }
             }
         }
     }
--- onboarding/onboarding_presentation/src/main/java/com/example/onboarding_presentation/welcome/WelcomeScreen.kt
+++ onboarding/onboarding_presentation/src/main/java/com/example/onboarding_presentation/welcome/WelcomeScreen.kt
@@ -9,13 +9,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import com.example.core.R
+import com.example.core.navigation.Route
+import com.example.core.util.UiEvent
 import com.example.core_ui.LocalSpacing
 import com.example.onboarding_presentation.component.ActionButton
 
 
 @Composable
-fun welcomeScreen() {
+fun WelcomeScreen(onNavigate : (UiEvent.Navigate) -> Unit) {
     val spacing = LocalSpacing.current
     Column(
         modifier = Modifier.padding(spacing.spaceMedium).fillMaxSize(),
@@ -29,7 +32,7 @@ fun welcomeScreen() {
             style = MaterialTheme.typography.h1
         )
         Spacer(modifier = Modifier.height(spacing.spaceMedium))
-        ActionButton(text = stringResource(id = R.string.next), onClick = { /*TODO*/ },
+        ActionButton(text = stringResource(id = R.string.next), onClick = { onNavigate(UiEvent.Navigate(Route.AGE))},
             modifier = Modifier.align(
             Alignment.CenterHorizontally
         ))
