d2a6ca786ca5b9b928a728c4134521e83ee8fd43
okezino <okezi002@gmail.com>
2022-10-28 19:16:23 +0100

    setup shared preference

---
+++ app/src/main/java/com/example/multimoduleapplication/di/AppModule.kt
@@ -0,0 +1,37 @@
+package com.example.multimoduleapplication.di
+
+import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
+import com.example.core.data.preferences.DefaultPreferences
+import com.example.core.domain.preferences.Preferences
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+    @Provides
+    @Singleton
+    fun provideSharedPreferences(
+        app: Application
+    ): SharedPreferences {
+        return app.getSharedPreferences("shared_pref", Context.MODE_PRIVATE)
+    }
+
+    @Provides
+    @Singleton
+    fun providePreferences(sharedPreferences: SharedPreferences): Preferences {
+        return DefaultPreferences(sharedPreferences)
+    }
+
+//    @Provides
+//    @Singleton
+//    fun provideFilterOutDigitsUseCase(): FilterOutDigits {
+//        return FilterOutDigits()
+//    }
+}
---
+++ app/src/main/java/com/example/multimoduleapplication/di/MultiModuleApp.kt
@@ -0,0 +1,5 @@
+package com.example.multimoduleapplication.di
+
+import android.app.Application
+
+class MultiModuleApp : Application()
