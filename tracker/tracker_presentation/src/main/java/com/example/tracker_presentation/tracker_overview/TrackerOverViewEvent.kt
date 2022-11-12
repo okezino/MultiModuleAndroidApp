938774d77d39eefc025d6a8a5c9e56bcf0eaaad7
okezino <okezi002@gmail.com>
2022-11-12 23:48:58 +0100

    drawable update

---
+++ tracker/tracker_presentation/src/main/java/com/example/tracker_presentation/tracker_overview/TrackerOverViewEvent.kt
@@ -0,0 +1,13 @@
+package com.example.tracker_presentation.tracker_overview
+
+import com.example.tracker_domain.model.TrackedFood
+
+sealed class TrackerOverViewEvent {
+
+    object OnNextDayClick: TrackerOverViewEvent()
+    object OnPreviousDayClick: TrackerOverViewEvent()
+    data class OnToggleMealClick(val meal: Meal): TrackerOverViewEvent()
+    data class OnDeleteTrackedFoodClick(val trackedFood: TrackedFood): TrackerOverViewEvent()
+
+
+}
